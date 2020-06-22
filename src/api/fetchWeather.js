3ec934412e04a9a3cb6a9128af70d00cda160a0f
Adithya Kamath <adithyakamath@Adithyas-MacBook-Air.local>
2020-06-23 02:53:00 +0530

    PWA app complete

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const URL = `https://api.openweathermap.org/data/2.5/weather`;
-const API_KEY= `2c5677fecbb40ee8ae1df7f5784ceca8`;
+const API_KEY= process.env.REACT_APP_API_KEY;
 export const fetchWeather = async(query)=>{
     const {data} = await axios.get(URL,{
         params:{
